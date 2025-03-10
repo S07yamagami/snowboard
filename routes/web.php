5875d62368ab37a0fb4118a6f70b7a2828f124cf
yamagami <shanshangmeng@gmail.com>
2025-03-10 19:46:12 +0900

    HTMLベース作成

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,20 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/home', function () {
+        return view('home');
+    });
+
+    Route::get('/about', function () {
+        return view('about');
+    });
+
+    Route::get('/style', function () {
+        return view('style');
+    });
+
+    
 });
 
 require __DIR__.'/auth.php';
