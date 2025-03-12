d6b2208d5d13620beb1cf846786f2f8303fdd49b
yamagami <shanshangmeng@gmail.com>
2025-03-12 21:54:09 +0900

    フッターにボタンを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,11 @@
             
         </div>
         <footer class="fixed bottom-0 w-full bg-gray-800 text-white py-4 text-center" style="bottom:0">
-            <p>ここはフッター</p>
+            <a href="/home" class="px-4 py-2 bg-blue-500 rounded">ホーム</a>
+            <a href="/about" class="px-4 py-2 bg-blue-500 rounded">ホームページについて</a>
+            <a href="/style" class="px-4 py-2 bg-blue-500 rounded">スタイル</a>
+
+
         </footer>
     </body>
 </html>
