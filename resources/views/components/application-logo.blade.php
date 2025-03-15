6e142ad5ea8cf948af36afec092316f135897c29
yamagami <shanshangmeng@gmail.com>
2025-03-15 14:59:41 +0900

    ロゴ　フェードイン

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,6 @@
-<img src="https://salomon.jp/cdn/shop/files/salomon-logo.png?v=1671255000&width=250" alt="">
+<div data-aos="fade-zoom-in"
+     data-aos-easing="ease-in-sine"
+     data-aos-delay="1000"
+     data-aos-offset="0">
+     <img src="https://salomon.jp/cdn/shop/files/salomon-logo.png?v=1671255000&width=250" alt="">
+</div>
